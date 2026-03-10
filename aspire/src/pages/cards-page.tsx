5e6dbfc97a990636f9ede1a7dcbdef8bb028a5a7
Afshankhan15 <afshan.khan.cs.2019@miet.ac.in>
2026-03-10 22:37:41 +0530

    added header section

--- aspire/src/pages/cards-page.tsx
+++ aspire/src/pages/cards-page.tsx
@@ -1,5 +1,27 @@
+import BoxIcon from "../assets/images/box@3x.png";
+
 const CardsPage = () => {
-  return <div>cards page</div>;
+  return (
+    <section className="p-16">
+      {/* HEADER SECTION */}
+      <header className="flex flex-col">
+        <p className="text-sm leading-xs">Available balance</p>
+
+        <div className="flex flex-wrap justify-between items-center mt-2.5">
+          <div className="flex gap-3 items-center">
+            <span className="text-xs leading-xs bg-aspire-green text-aspire-white px-3 py-1 rounded-sm">
+              S$
+            </span>
+            <span className="text-title leading-xs font-bold">3,000</span>
+          </div>
+          <button className="flex bg-aspire-blue text-aspire-white font-bold items-center gap-2 px-3 py-2 rounded-md">
+            <img className="h-5 w-5" src={BoxIcon} alt="box icon" />
+            <span className="text-xs leading-sm">New card</span>
+          </button>
+        </div>
+      </header>
+    </section>
+  );
 };
 
 export default CardsPage;
